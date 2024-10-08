9ebf6e575336057995510e03f6e1e20ee2e4e6c5
Nitu Kumari <nitukumari@Nitus-MacBook-Air.local>
2024-10-08 10:16:32 +0530

    final build

---
+++ out/_next/static/xF_ggCsJ5Bom1aBJlFtVE/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fagency-details\u002F[id]","\u002Fagent-details\u002F[id]","\u002Fblog-details\u002F[id]","\u002Flisting-details-v1\u002F[id]","\u002Flisting-details-v2\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
